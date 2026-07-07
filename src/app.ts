6a848cd6d417e00e31b26ea158765f327ba7eca0
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-07-07 12:07:56 -0300

    feat(server): enable CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 export { app };
