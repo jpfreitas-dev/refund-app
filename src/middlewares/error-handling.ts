1b04173f4f16fdb5e5518273ab868f095192ea3e
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-07-09 17:21:26 -0300

    feat(server): add error handling middleware and update dependencies

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,29 @@
+import { AppError } from '@/utils/AppError';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+
+export const errorHandling: ErrorRequestHandler = (
+  error,
+  _request,
+  response,
+  _next,
+) => {
+  if (error instanceof AppError) {
+    response.status(error.statusCode).json({ message: error.message });
+    return;
+  }
+
+  if (error instanceof ZodError) {
+    response.status(400).json({
+      message: 'Validation error',
+      issues: error.format(),
+    });
+    return;
+  }
+
+  response.status(500).json({
+    message: 'Internal server error',
+  });
+
+  return;
+};
